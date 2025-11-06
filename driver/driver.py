297c085b9f9f240a27d83e35042cf8fd558ce891
kwilli29 <kwilli29@nd.edu>
2025-11-06 11:12:50 -0500

    update comments

--- driver/driver.py
+++ driver/driver.py
@@ -7,7 +7,7 @@ from packet import Packet
 #### GLOBAL VARIABLES ####
 
 cxn_timeout 		 = 120000		# Connection Timeout -- try and connect to any device for 2 min -- placeholder value 
-poll_sensor_window = 4 				# get reading from sensor every x units -- placeholder value
+poll_sensor_window  = 4 			# get reading from sensor every x units -- placeholder value
 check_cxn_timer 	 = 60000 		# check connection every 60 s, 60000 ms -- placeholder value
 
 ringbuffer = 0
@@ -16,7 +16,7 @@ ringbuffer = 0
 
 def init_wifi():  # Initialize Wifi Comms Protocol
 	return 1
-def init_bt(): 	# Initialize Bluetooth Comms Protocol
+def init_bt(): 	 # Initialize Bluetooth Comms Protocol
 	return 0
 def init_radio(): # Initialize Radio Comms Protocol
 	return 0
@@ -43,7 +43,6 @@ def init_connect(cxn_type=0):
 		if(init_plug() == 1):
 			ready = 1
 
-
 	return ready
 
 def choose_cxn_device(cxn_type=0):
@@ -51,12 +50,12 @@ def choose_cxn_device(cxn_type=0):
 	## Try and Connect to a Device ##
 
 	# Could design a spin wait / polling to try and connect within a time window
-	if(init_connect(cxn_type) == 0): # Device tried did not connect
-		if(init_connect(0) == 0):     # Try connecting to all devices if first attempt fails
-			if(init_connect(1) == 0):
-				if(init_connect(2) == 0):
-					if(init_connect(3) == 0):
-						cxn_type = -1 # Could not connect to a device
+	if(init_connect(cxn_type) == 0): # Device tried did not connect -- Try connecting to all devices if first attempt fails
+		if(init_connect(0) == 0):     # Try Wifi
+			if(init_connect(1) == 0):	# Try BT
+				if(init_connect(2) == 0):		# Try Radio
+					if(init_connect(3) == 0):	# Try Pi w/ Wired Cxn
+						cxn_type = -1 				# Could not connect to any device
 					else:
 						cxn_type = 3 # Cxd to Pi w/ Wired Cxn
 				else:
@@ -95,6 +94,7 @@ def main():
 	## Begin Sampling Period ##
 
 	# get and store data from sensor
+	# format / possibly denoise
 	# perioidcally send to Pi
 
 	state = 'Recv'
@@ -110,15 +110,14 @@ def main():
 	#			# accumulate into a data structure
 	#			# format
 
-	# 		elif state == 'Denoise':
-	#			# denoise chunk of data
-
 	#		elif state == 'Send':
 	#			# send data to Pi
 	#			# Receive ACK from Pi, else Try to Send Again [check cxn, etc.]
 	#			# state = 'Recv'
 
-	#
+	#		else:
+	#			state = 'Recv'
+	#			poll_sensor_window = 0
 
 	return 
 
@@ -127,12 +126,14 @@ def main():
 # make communication easy to “switch out” if possible
 # have an initial window of specific demographic and sampling baseline data before streaming data
 #	  I forget data rate but sensor might sample at like 4hz or something similar
-# leave a “denoising” chunk between receiving from sensor and sending it to model
+# leave a “denoising” chunk between receiving from sensor and sending it to model possibly
 # collect & send data every x time
 # packets – fixed sized packets
 # check connection periodically, or if message send or receive fails enough, attempt to switch to a new comms
 # account for not knowing demographics [ data noise / "attack"? ] 
 # ringbuffer
+
+# data preprocessing is probably best left to the Pi, so minimal processing should be done I think
 
 
 if __name__ == '__main__':
